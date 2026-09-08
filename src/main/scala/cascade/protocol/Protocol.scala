a76137c8cadce04ce208c8ed15fb91c2cae2fce8
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2026-09-08 14:53:43 +0330

    advertise DeleteGroups v0 and v1

--- src/main/scala/cascade/protocol/Protocol.scala
+++ src/main/scala/cascade/protocol/Protocol.scala
@@ -126,6 +126,7 @@ object Compatibility:
     ApiVersion(ApiKey.CreateAcls, 1, 1),
     ApiVersion(ApiKey.DeleteAcls, 1, 1),
     ApiVersion(ApiKey.DescribeConfigs, 2, 2),
+    ApiVersion(ApiKey.DeleteGroups, 0, 1),
     ApiVersion(ApiKey.IncrementalAlterConfigs, 0, 0),
     ApiVersion(ApiKey.SaslAuthenticate, 1, 1),
     ApiVersion(ApiKey.AlterPartitionReassignments, 0, 0),
