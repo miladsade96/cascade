39426ed0c66b5064e103865163894552b044b680
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2026-09-08 14:51:48 +0330

    advertise DescribeGroups v0 through v4

--- src/main/scala/cascade/protocol/Protocol.scala
+++ src/main/scala/cascade/protocol/Protocol.scala
@@ -112,6 +112,7 @@ object Compatibility:
     ApiVersion(ApiKey.Heartbeat, 3, 3),
     ApiVersion(ApiKey.LeaveGroup, 2, 2),
     ApiVersion(ApiKey.SyncGroup, 3, 3),
+    ApiVersion(ApiKey.DescribeGroups, 0, 4),
     ApiVersion(ApiKey.ListGroups, 0, 4),
     ApiVersion(ApiKey.SaslHandshake, 1, 1),
     ApiVersion(ApiKey.ApiVersions, 0, 4),
