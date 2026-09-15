85d87213d5520122a4e0c6ededaaa1ddb12ac75f
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2026-09-15 10:35:37 +0330

    extend the frozen internal API boundary for quota reservations

--- src/test/scala/cascade/coordinator/CoordinatorQuorumRequestCodecSuite.scala
+++ src/test/scala/cascade/coordinator/CoordinatorQuorumRequestCodecSuite.scala
@@ -18,7 +18,8 @@ final class CoordinatorQuorumRequestCodecSuite extends FunSuite:
     assert(InternalApi.contains(InternalApi.CoordinatorShardRecover))
     assert(InternalApi.contains(InternalApi.CoordinatorDecisionQuery))
     assert(InternalApi.contains(InternalApi.CoordinatorStateQuery))
-    assert(!InternalApi.contains((InternalApi.CoordinatorStateQuery - 1).toShort))
+    assert(InternalApi.contains(InternalApi.QuotaReserve))
+    assert(!InternalApi.contains((InternalApi.QuotaReserve - 1).toShort))
   }
 
   test("rejects trailing and negative-term requests") {
